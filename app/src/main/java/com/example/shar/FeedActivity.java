898c72dc5cbc979ad93cb7b5683b6dc7c2c6ac58
lily <lrcgal@gmail.com>
2019-05-01 16:31:20 -0400

    fix feed activity

--- app/src/main/java/com/example/shar/FeedActivity.java
+++ app/src/main/java/com/example/shar/FeedActivity.java
@@ -3,8 +3,15 @@ package com.example.shar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
 
 
 public class FeedActivity extends AppCompatActivity {
@@ -14,6 +21,13 @@ public class FeedActivity extends AppCompatActivity {
     public static final String USERNAME_KEY = "USERNAME_KEY";
     private String mUID;
     private String mUserName;
+    private PostRecyclerView mRecyclerView;
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +82,73 @@ public class FeedActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    private void initRecyclerView(){
+
+
+        DataBaseHelper dbh = new DataBaseHelper(mUID);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        //VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        //mRecyclerView.addItemDecoration(itemDecorator);
+
+        dbh.readPosts(new DataBaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(ArrayList<Post> posts, ArrayList<String> keys) {
+                ArrayList<Post> mediaObjects = posts;
+                mRecyclerView.setMediaObjects(mediaObjects);
+                PostAdapter adapter = new PostAdapter(mediaObjects, initGlide());
+                mRecyclerView.setAdapter(adapter);
+
+
+
+            }
+            @Override
+            public  void DataIsLoaded(String username){
+
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUploaded() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
+
+
+
+    }
+
+    private RequestManager initGlide(){
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.drawable.white_background)
+                .error(R.drawable.white_background);
+
+        return Glide.with(this)
+                .setDefaultRequestOptions(options);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if(mRecyclerView!=null)
+            mRecyclerView.releasePlayer();
+        super.onDestroy();
+    }
+
+
+
 
 
 }
